bb9d81c6fb8d92c12e9b60fc8a46556fbe87eaa1
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2026-05-28 01:10:00 +0100

    Setup app icon

--- Launcher/resource.h
+++ Launcher/resource.h
@@ -1,7 +1,7 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
-
+#define BW_CSHARP 104
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
