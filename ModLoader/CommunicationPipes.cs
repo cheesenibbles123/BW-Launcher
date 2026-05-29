5d93bb5e5186e77503177d5817687351d406cb1d
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2026-05-29 20:51:26 +0100

    Made communication pipes more robust

--- ModLoader/CommunicationPipes.cs
+++ ModLoader/CommunicationPipes.cs
@@ -1,39 +1,80 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Tools
 {
     public class CommunicationPipes
     {
-        private static TcpClient clientSocket;
+        private static TcpClient clientSocket = null;
 
         public static void init()
         {
-            clientSocket = new TcpClient();
-            clientSocket.Connect("127.0.0.1", 54000);
+            if (clientSocket == null || !clientSocket.Connected)
+            {
+                if (clientSocket == null)
+                {
+                    clientSocket = new TcpClient();
+                }
 
-            sendMessage("Hello from game client!");
+                clientSocket.Connect("127.0.0.1", 54000);
+
+                AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+
+                sendMessage("Hello from game client!");
+            } else
+            {
+                UnityEngine.Debug.Log("Called CommunicationPipes.init() with a socket already present");
+            }
         }
 
-        public static void sendMessage(string message)
+        /// <returns> Whether the message was successfully sent or not </returns>
+        public static bool sendMessage(string message)
         {
-            UnityEngine.Debug.Log("Sending message to launcher: " + message);
-            NetworkStream stream = clientSocket.GetStream();
-            byte[] data = Encoding.UTF8.GetBytes(message);
-            stream.Write(data, 0, data.Length);
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                UnityEngine.Debug.Log("Sending message to launcher: " + message);
+                NetworkStream stream = clientSocket.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                return true;
+            }
+
+            return false;
         }
 
-        public static void incrementAchievement(string modId, string achievementId, int progressValue)
+        /**
+         * <summary>
+         * Currently "stats" require an achievement hooked up to them to function
+         * </summary>
+         * <returns> Whether the message was successfully sent or not </returns>
+         */
+        public static bool incrementAchievement(string modId, string achievementId, int progressValue)
         {
-            UnityEngine.Debug.Log("Sending achievement update to launcher: " + achievementId + " " + progressValue);
-            NetworkStream stream = clientSocket.GetStream();
-            byte[] data = Encoding.UTF8.GetBytes(string.Format("[ACHIEVEMENT][{0}][{1}][{2}]", modId, achievementId, progressValue));
-            stream.Write(data, 0, data.Length);
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                UnityEngine.Debug.Log("Sending achievement update to launcher: " + achievementId + " " + progressValue);
+                NetworkStream stream = clientSocket.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(string.Format("[ACHIEVEMENT][{0}][{1}][{2}]", modId, achievementId, progressValue));
+                stream.Write(data, 0, data.Length);
+                return true;
+            }
+
+            return false;
         }
 
-        public void terminate()
+        public static void CloseConnection()
         {
-            clientSocket.Close();
+            if (clientSocket != null && clientSocket.Connected)
+            {
+                clientSocket.Close();
+            }
+        }
+
+        private static void OnProcessExit(object sender, EventArgs e)
+        {
+            CloseConnection();
+            clientSocket = null;
         }
     }
 }
